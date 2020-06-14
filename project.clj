0f773e730b3295e837f8c16d6a6fa8c0994606e6
Jona Ekenberg <saikyun@gmail.com>
2020-06-14 20:37:00 +0200

    no java left

--- project.clj
+++ project.clj
@@ -2,10 +2,10 @@
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :source-paths ["src"]
   ;;:java-source-paths ["src"]
-  :resource-paths [#_"extra-classes" "classes"]
+  :resource-paths [#_"extra-classes" #_"classes"]
   :jvm-opts ["-Dclojure.compiler.direct-linking=true"
              "-Dclojure.spec.skip-macros=true"
-             ;;"-Dclojure.server.repl={:port 5555 :accept clojure.core.server/repl}"
+             "-Dclojure.server.repl={:port 5555 :accept clojure.core.server/repl}"
              ]
   :aot :all
   :profiles {:clojure-1.10.2-alpha1 {:dependencies [[org.clojure/clojure "1.10.2-alpha1"]]}
