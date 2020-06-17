8649178b7d318a5c5dccd36d7e80c01c4d8c83e5
Jona Ekenberg <saikyun@gmail.com>
2020-06-17 14:57:26 +0200

    moved the c code a bit

--- src/create_sdl_ns.clj
+++ src/create_sdl_ns.clj
@@ -1,6 +1,62 @@
 (ns create-sdl-ns
   (:require [spec-c-lib :as scl]))
 
+(def functions
+  ["int GET_SDL_INIT_VIDEO() { return SDL_INIT_VIDEO; }"
+   
+   "int GET_SDL_WINDOW_SHOWN() { return SDL_WINDOW_SHOWN; }"
+   "char *gen_title() { return \"SDL Tutorial\"; }"
+   
+   "
+SDL_Rect *create_rect(int x, int y, int w, int h) {
+  SDL_Rect *r = (SDL_Rect*)malloc(sizeof(SDL_Rect));
+  r->x = x;
+  r->y = y;
+  r->w = w;
+  r->h = h;
+  return r;
+}"
+   
+   "
+SDL_Event e;
+
+SDL_Event *get_e() {
+  return &e;
+}"])
+
+(def prototypes
+  ["int SDL_Init(Uint32 flags)"
+   "int SDL_PollEvent(SDL_Event* event)"
+   "void SDL_Delay(Uint32 ms)"
+   "int SDL_UpdateWindowSurface(SDL_Window* window)"
+   "SDL_Surface* SDL_GetWindowSurface(SDL_Window* window)"
+   
+   "
+Uint32 SDL_MapRGB(const SDL_PixelFormat* format,
+                  Uint8                  r, 
+                  Uint8                  g, 
+                  Uint8                  b)
+"
+   
+   "
+SDL_Window* SDL_CreateWindow(const char* title,
+                             int         x,
+                             int         y,
+                             int         w,
+                             int         h,
+                             Uint32      flags)
+"
+   
+   "
+int SDL_FillRect(SDL_Surface*    dst,
+                 const SDL_Rect* rect,
+                 Uint32          color)
+"
+   
+   {:ret "void", :sym "SDL_Quit"} ;; you can also provide prototypes as data
+   
+   ])
+
 
 (defn -main
   []
@@ -12,48 +68,8 @@
   (println "Generating c.sdl")
   (scl/gen-and-persist
    'c.sdl
-   {:functions ["int GET_SDL_INIT_VIDEO() { return SDL_INIT_VIDEO; }"
-                "int GET_SDL_WINDOW_SHOWN() { return SDL_WINDOW_SHOWN; }"
-                "char *gen_title() { return \"SDL Tutorial\"; }"
-                "SDL_Rect *create_rect(int x, int y, int w, int h) {
-  SDL_Rect *r = (SDL_Rect*)malloc(sizeof(SDL_Rect));
-  r->x = x;
-  r->y = y;
-  r->w = w;
-  r->h = h;
-  return r;
-}"
-                
-                " SDL_Event e;
-SDL_Event *get_e() {
-  return &e;
-}"
-                
-                ]
-    :protos ["int SDL_Init(Uint32 flags)"
-             "int SDL_PollEvent(SDL_Event* event)"
-             "void SDL_Delay(Uint32 ms)"
-             "int SDL_UpdateWindowSurface(SDL_Window* window)"
-             "SDL_Surface* SDL_GetWindowSurface(SDL_Window* window)"
-             "
-Uint32 SDL_MapRGB(const SDL_PixelFormat* format,
-                  Uint8                  r, 
-                  Uint8                  g, 
-                  Uint8                  b)"
-             "
-SDL_Window* SDL_CreateWindow(const char* title,
-                             int         x,
-                             int         y,
-                             int         w,
-                             int         h,
-                             Uint32      flags)"
-             "
-int SDL_FillRect(SDL_Surface*    dst,
-                 const SDL_Rect* rect,
-                 Uint32          color)"
-             
-             
-             {:ret "void", :sym "SDL_Quit"}]
+   {:functions functions
+    :protos prototypes
     :includes ["stdio.h" "SDL2/SDL.h"]
     :c-path "src/generated.c"
     :bc-path "libs/generated.bc"
