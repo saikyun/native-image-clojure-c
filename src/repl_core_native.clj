95247c88c0b31dfb2686026507e9e01ed57b5b01
Jona Ekenberg <saikyun@gmail.com>
2020-06-19 11:04:14 +0200

    currently generated code works. need to move interfaces + generate more functions

--- src/repl_core_native.clj
+++ src/repl_core_native.clj
@@ -7,41 +7,42 @@
 (defn -main [& args]
   (println "lul")
   
-  (sdl/init (sdl/get-sdl-init-video))
+  (sdl_native_lib/init 0 #_ (sdl/get-sdl-init-video))
   
-  
-  (let [window (sdl_native_lib/create_window (sdl_native_lib/gen_title)
-                                             0
-                                             0
-                                             640
-                                             480
-                                             (sdl/get-sdl-window-shown))
-        screen (sdl_native_lib/get_window_surface window)
-        rect (sdl_native_lib/create_rect 0 0 100 50)
-        format (.format screen)]
-    (println "format" (.rawValue format))
+  (comment
     
-    #_(sdl_native_lib/fill_rect screen nil (sdl_native_lib/map_rgb (.format screen) 0xFF 0xFF 0xFF))
-    (sdl_native_lib/fill_rect screen (sdl_native_lib/get_null) ;; just nil doesn't work for ni
-                              (sdl_native_lib/map_rgb (.format screen) 0xFF 0xFF 0xFF))
-    (sdl_native_lib/fill_rect screen rect (sdl_native_lib/map_rgb (.format screen) 0xFF 0 0))
-    
-    (sdl_native_lib/update_window_surface window)
+    (let [window (sdl_native_lib/create_window (sdl_native_lib/gen_title)
+                                               0
+                                               0
+                                               640
+                                               480
+                                               (sdl/get-sdl-window-shown))
+          screen (sdl_native_lib/get_window_surface window)
+          rect (sdl_native_lib/create_rect 0 0 100 50)
+          format (.format screen)]
+      (println "format" (.rawValue format))
+      
+      #_(sdl_native_lib/fill_rect screen nil (sdl_native_lib/map_rgb (.format screen) 0xFF 0xFF 0xFF))
+      (sdl_native_lib/fill_rect screen (sdl_native_lib/get_null) ;; just nil doesn't work for ni
+                                (sdl_native_lib/map_rgb (.format screen) 0xFF 0xFF 0xFF))
+      (sdl_native_lib/fill_rect screen rect (sdl_native_lib/map_rgb (.format screen) 0xFF 0 0))
+      
+      (sdl_native_lib/update_window_surface window)
 
-    (println "rgb1" (sdl_native_lib/map_rgb (.format screen) 0xFF 0xFF 0xFF))
-    (println "rgb2" (sdl_native_lib/map_rgb (.format screen) 0xFF 0 0))
+      (println "rgb1" (sdl_native_lib/map_rgb (.format screen) 0xFF 0xFF 0xFF))
+      (println "rgb2" (sdl_native_lib/map_rgb (.format screen) 0xFF 0 0))
+      
+      (loop [quit false]
+        (let [quit (when-not (= 0 (sdl_native_lib/poll_event (sdl_native_lib/get_e)))
+                     (when (= 256 (.type (sdl_native_lib/get_e)))
+                       true))]
+          (if quit
+            :quit
+            (recur quit))))
+      
+      (println "window" (.rawValue window))
+      (println "screen" (.rawValue screen)))
     
-    (loop [quit false]
-      (let [quit (when-not (= 0 (sdl_native_lib/poll_event (sdl_native_lib/get_e)))
-                   (when (= 256 (.type (sdl_native_lib/get_e)))
-                     true))]
-        (if quit
-          :quit
-          (recur quit))))
-    
-    (println "window" (.rawValue window))
-    (println "screen" (.rawValue screen)))
-  
-  (sdl_native_lib/sdl_quit)
+    (sdl_native_lib/sdl_quit))
   
   )
--- src/sdl_native.clj
+++ src/sdl_native.clj
@@ -1,5 +1,7 @@
 (ns sdl-native
-  (:require [patch-gen-class :as pgc])
+  (:require [patch-gen-class :as pgc]
+            [gen-clj.native-image :as ni]
+            [gen-clj :as gclj :refer [gen-clojure-mapping]])
   (:import org.graalvm.word.PointerBase
            org.graalvm.nativeimage.c.struct.CField
            org.graalvm.nativeimage.c.CContext
@@ -11,18 +13,13 @@
            org.graalvm.nativeimage.c.function.CFunction
            org.graalvm.word.WordFactory
            
-           [org.graalvm.nativeimage.c.type CCharPointer VoidPointer])
+           [org.graalvm.nativeimage.c.type CCharPointer VoidPointer])  
   (:gen-class))
 
-(deftype Headers
-    []
-  org.graalvm.nativeimage.c.CContext$Directives
-  (getHeaderFiles
-    [this]
-    [(str "\"" (System/getProperty "user.dir") "/src/generated.h\"")]))
+(require 'gen_sdl_native_lib)
 
 (gen-interface 
- :name ^{org.graalvm.nativeimage.c.CContext sdl_native.Headers
+ :name ^{org.graalvm.nativeimage.c.CContext gen_sdl_native_lib.Headers
          org.graalvm.nativeimage.c.function.CLibrary "generated"
          org.graalvm.nativeimage.c.struct.CStruct "SDL_Event"}
  sdl_native.SDL_Event
@@ -32,7 +29,7 @@
             int]])
 
 (gen-interface 
- :name ^{org.graalvm.nativeimage.c.CContext sdl_native.Headers
+ :name ^{org.graalvm.nativeimage.c.CContext gen_sdl_native_lib.Headers
          org.graalvm.nativeimage.c.function.CLibrary "generated"
          org.graalvm.nativeimage.c.struct.CStruct "SDL_PixelFormat"}
  sdl_native.SDL_PixelFormat
@@ -42,7 +39,7 @@
             org.graalvm.nativeimage.c.type.VoidPointer]])
 
 (gen-interface 
- :name ^{org.graalvm.nativeimage.c.CContext sdl_native.Headers
+ :name ^{org.graalvm.nativeimage.c.CContext gen_sdl_native_lib.Headers
          org.graalvm.nativeimage.c.function.CLibrary "generated"
          org.graalvm.nativeimage.c.struct.CStruct "SDL_Surface"}
  sdl_native.SDL_Surface
@@ -52,115 +49,120 @@
             []
             sdl_native.SDL_PixelFormat]])
 
-(pgc/gen-class-native
- :name ^{org.graalvm.nativeimage.c.CContext sdl_native.Headers
-         org.graalvm.nativeimage.c.function.CLibrary "generated"}
- sdl_native_lib
- 
- :methods [^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
-                                {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION}}
-                              _SHADOWING_SDL_Init
-                              [int]
-                              void]
+#_(def types
+    {"void" 'void
+     "int" 'int
+     "Uint32" 'int})
 
-           ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
-                                {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION
-                                 :value "_SHADOWING_SDL_Quit"}}
-                              sdl_quit
-                              []
-                              void]
-           
-           ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
-                                {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION}}
-                              GET_SDL_INIT_VIDEO
-                              []
-                              int]
-           
-           ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
-                                {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION}}
-                              GET_SDL_WINDOW_SHOWN
-                              []
-                              int]
-           
-           ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
-                                {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION}}
-                              gen_title
-                              []
-                              org.graalvm.nativeimage.c.type.CCharPointer]
-           
-           ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
-                                {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION
-                                 :value "_SHADOWING_SDL_CreateWindow"}}
-                              create_window
-                              [org.graalvm.nativeimage.c.type.CCharPointer, int, int, int, int, int]
-                              org.graalvm.nativeimage.c.type.VoidPointer
-                              #_int]
-           
-           ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
-                                {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION
-                                 :value "_SHADOWING_SDL_GetWindowSurface"}}
-                              get_window_surface
-                              [org.graalvm.nativeimage.c.type.VoidPointer]
-                              sdl_native.SDL_Surface]
-           
-           ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
-                                {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION}}
-                              create_rect
-                              [int int int int]
-                              org.graalvm.nativeimage.c.type.VoidPointer]
-           
-           ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
-                                {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION
-                                 :value "_SHADOWING_SDL_MapRGB"}}
-                              map_rgb
-                              [org.graalvm.nativeimage.c.type.VoidPointer #_ sdl_native.SDL_PixelFormat int int int]
-                              int]
-
-           ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
-                                {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION
-                                 :value "_SHADOWING_SDL_FillRect"}}
-                              fill_rect
-                              [org.graalvm.nativeimage.c.type.VoidPointer
-                               org.graalvm.nativeimage.c.type.VoidPointer
-                               int]
-                              int]
-           
-           
-           ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
-                                {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION}}
-                              get_null
-                              []
-                              org.graalvm.nativeimage.c.type.VoidPointer]
-           
-           ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
-                                {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION
-                                 :value "_SHADOWING_SDL_PollEvent"}}
-                              poll_event
-                              [org.graalvm.nativeimage.c.type.VoidPointer]
-                              int]
-           
-           ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
-                                {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION}}
-                              get_e
-                              []
-                              sdl_native.SDL_Event]
-
-
-           ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
-                                {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION
-                                 :value "_SHADOWING_SDL_UpdateWindowSurface"}}
-                              update_window_surface
-                              [org.graalvm.nativeimage.c.type.VoidPointer]
-                              int]])
+#_(pgc/gen-class-native
+   :name ^{org.graalvm.nativeimage.c.CContext sdl_native.Headers
+           org.graalvm.nativeimage.c.function.CLibrary "generated"}
+   sdl_native_lib
+   
+   :methods [^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
+                                  {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION}}
+                                _SHADOWING_SDL_Init
+                                [int]
+                                void]
+             
+             ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
+                                  {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION
+                                   :value "_SHADOWING_SDL_Quit"}}
+                                sdl_quit
+                                []
+                                void]
+             
+             ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
+                                  {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION}}
+                                GET_SDL_INIT_VIDEO
+                                []
+                                int]
+             
+             ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
+                                  {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION}}
+                                GET_SDL_WINDOW_SHOWN
+                                []
+                                int]
+             
+             ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
+                                  {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION}}
+                                gen_title
+                                []
+                                org.graalvm.nativeimage.c.type.CCharPointer]
+             
+             ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
+                                  {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION
+                                   :value "_SHADOWING_SDL_CreateWindow"}}
+                                create_window
+                                [org.graalvm.nativeimage.c.type.CCharPointer, int, int, int, int, int]
+                                org.graalvm.nativeimage.c.type.VoidPointer
+                                #_int]
+             
+             ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
+                                  {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION
+                                   :value "_SHADOWING_SDL_GetWindowSurface"}}
+                                get_window_surface
+                                [org.graalvm.nativeimage.c.type.VoidPointer]
+                                sdl_native.SDL_Surface]
+             
+             ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
+                                  {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION}}
+                                create_rect
+                                [int int int int]
+                                org.graalvm.nativeimage.c.type.VoidPointer]
+             
+             ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
+                                  {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION
+                                   :value "_SHADOWING_SDL_MapRGB"}}
+                                map_rgb
+                                [org.graalvm.nativeimage.c.type.VoidPointer #_ sdl_native.SDL_PixelFormat int int int]
+                                int]
+             
+             ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
+                                  {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION
+                                   :value "_SHADOWING_SDL_FillRect"}}
+                                fill_rect
+                                [org.graalvm.nativeimage.c.type.VoidPointer
+                                 org.graalvm.nativeimage.c.type.VoidPointer
+                                 int]
+                                int]
+             
+             
+             ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
+                                  {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION}}
+                                get_null
+                                []
+                                org.graalvm.nativeimage.c.type.VoidPointer]
+             
+             ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
+                                  {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION
+                                   :value "_SHADOWING_SDL_PollEvent"}}
+                                poll_event
+                                [org.graalvm.nativeimage.c.type.VoidPointer]
+                                int]
+             
+             ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
+                                  {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION}}
+                                get_e
+                                []
+                                sdl_native.SDL_Event]
+             
+             
+             ^:static ^:native [^{org.graalvm.nativeimage.c.function.CFunction
+                                  {:transition org.graalvm.nativeimage.c.function.CFunction$Transition/NO_TRANSITION
+                                   :value "_SHADOWING_SDL_UpdateWindowSurface"}}
+                                update_window_surface
+                                [org.graalvm.nativeimage.c.type.VoidPointer]
+                                int]])
 
 (defn null
   []
   (org.graalvm.word.WordFactory/nullPointer))
-
-(defn init [flags] (sdl_native_lib/_SHADOWING_SDL_Init flags))
-(defn get-sdl-init-video [] (sdl_native_lib/GET_SDL_INIT_VIDEO))
-(defn get-sdl-window-shown [] (sdl_native_lib/GET_SDL_WINDOW_SHOWN))
-(defn gen-title ^org.graalvm.nativeimage.c.type.CCharPointer [] (sdl_native_lib/gen_title))
+(comment
+  (defn init [flags] (sdl_native_lib/_SHADOWING_SDL_Init flags))
+  (defn get-sdl-init-video [] (sdl_native_lib/GET_SDL_INIT_VIDEO))
+  (defn get-sdl-window-shown [] (sdl_native_lib/GET_SDL_WINDOW_SHOWN))
+  (defn gen-title ^org.graalvm.nativeimage.c.type.CCharPointer [] (sdl_native_lib/gen_title)))
 #_(defn create-window [title x y w h flag]
     (sdl_native_lib/_SHADOWING_SDL_CreateWindow title x y w h flag))
 
